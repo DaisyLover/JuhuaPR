70693e4ae2fb6a88451e7fc4fa069c39b47f3f2f
Ziyu Ji <jiziy.cn@gmail.com>
2014-02-06 22:17:21 -0500

    Added project documentation to "doc" folder.

--- src/job1/AdjacencyGraphMapper.java
+++ src/job1/AdjacencyGraphMapper.java
@@ -1,7 +1,20 @@
 package job1;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+
 /**
  * Created by Ziyu on 2/6/14.
  */
-public class AdjacencyGraphMapper {
+public class AdjacencyGraphMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+    @Override
+    public void map(LongWritable longWritable, Text text, OutputCollector<Text, Text> textTextOutputCollector, Reporter reporter) throws IOException {
+
+    }
 }
